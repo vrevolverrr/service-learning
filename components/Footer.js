19cf2608cb8d88ae28d1508d6ef27d3e9c4bd651
Bryan Soong <whiterevolver35@gmail.com>
2021-09-11 17:19:54 +0800

    :sparkles: fixed footer font

--- components/Footer.js
+++ components/Footer.js
@@ -2,13 +2,13 @@ import styles from '../styles/footer.module.css';
 
 export default function Footer() {
     return (
-        <footer className={styles.footer}>
+        <div className={styles.footer}>
             <div style={{display: 'flex', flexDirection: 'row', marginBottom: '20px'}}>
                 <p className={styles.item}>Contact Us</p>
                 <p className={styles.item}>About Us</p>
                 <p className={styles.item}>Attribution</p>
             </div>
-            <p style={{fontSize: '14px'}}>made with ♥ MCKL SL G8</p>
-        </footer>
+            <p style={{fontSize: '14px'}}>made with ♥ by MCKL SL G8</p>
+        </div>
     );
 }
