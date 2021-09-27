26c8d33e4091a6e6ac78ac8a9df9e9c4e63376d7
Bryan Soong <whiterevolver35@gmail.com>
2021-09-27 16:32:31 +0800

    :sparkles: added new stuff

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
   return (
     <>
         <Head>
-            <title>Service Learning</title>
+            <title>Carbon Zero - MCKL Service Learning</title>
             <meta name="description" content="Carbon Zero is a service learning project carried out by students of MCKL to raise awareness on carbon footprint." />
             <meta name="keywords" content="environment, carbon footprint, save the planet" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
