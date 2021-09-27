24859d951ae2d3ce93c523d754165e6743a4ec38
Bryan Soong <whiterevolver35@gmail.com>
2021-09-27 18:02:23 +0800

    :sparkles: added contact form

---
+++ pages/api/email.js
@@ -0,0 +1,35 @@
+import nodemailer from "nodemailer";
+
+export default async function main(req, res) {
+    if (req.method != 'POST') return;
+
+    const transporter = nodemailer.createTransport({
+        port: 465,
+        host: "smtp.gmail.com",
+        auth: {
+            user: "bryansoongdev@gmail.com",
+            pass: "0392bsjsdev",
+        },
+        secure: true,
+    });
+
+    const mailData = {
+        from: req.body.email,
+        to: "whiterevolver35@gmail.com",
+        subject: `Carbon Zero Contact Form`,
+        text: req.body.message + `\n\nBest Regards,\n${req.body.name}`,
+    };
+    
+    await new Promise((resolve, reject) => {
+        transporter.sendMail(mailData, (err, info) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                console.log(info);
+                resolve(info);
+            }
+        });
+    });
+    res.status(200);
+};
--- pages/contact.js
+++ pages/contact.js
@@ -1,18 +1,59 @@
+import { useRef } from 'react';
 import Banner from '../components/Banner';
 import Section from '../components/Section';
 import styles from '../styles/Contact.module.css';
 
 export default function Contact() {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const messageRef = useRef();
+
+    const onSubmit = async () => {
+        const payload = {
+            name: nameRef.current.value.trim(),
+            email: emailRef.current.value.trim(),
+            message: messageRef.current.value.trim(),
+        }
+
+        const response = await fetch('/api/email', {
+            method: 'POST',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+
+        console.log(response)
+    }
+
     return (
         <main className={styles.main}>
             <Banner>CONTACT US</Banner>
 
             <div style={{height: '3vh'}}/>
             
-            <Section title="Have A Question?" description="Feel free to contact us for any enquiries through the 
-            following:">
+            <Section height='5vh' title="Have A Question?" description="Feel free to contact us for any enquiries.">
+                <div style={{display: 'flex', flexDirection: 'column', width: '100%', maxWidth: '550px', margin: '10px'}}>
+                    <p className={styles.contact_label}>Name</p>
+                    <input ref={nameRef} className={styles.contact_field} type="text" placeholder="John Appleseed"/>
+                    
+                    <div style={{height: '25px'}}/>
 
+                    <p className={styles.contact_label}>Email</p>
+                    <input ref={emailRef} className={styles.contact_field} type="email" placeholder="john@example.com"/>
+
+                    <div style={{height: '25px'}}/>
+
+                    <p className={styles.contact_label}>Message</p>
+                    <textarea ref={messageRef} className={styles.contact_field} rows="6" placeholder="Your message"/>
+
+                    <div style={{height: '25px'}}/>
+                    
+                    <div style={{textAlign: 'right'}}><button onClick={onSubmit} className={styles.contact_submit}>Submit</button></div>
+                </div>
             </Section>
+            <div style={{height: '4vh'}}/>
         </main>
     );
 }
