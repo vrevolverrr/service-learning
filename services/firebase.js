bd19e38ee2d970fd8bd71afcb80a7d0e26709161
Bryan Soong <whiterevolver35@gmail.com>
2021-09-27 16:09:40 +0800

    :sparkles: added new stuff

--- services/firebase.js
+++ services/firebase.js
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app"
 import { getFirestore, doc, getDoc, updateDoc, increment } from "firebase/firestore"
 
 const config = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+    apiKey: "AIzaSyBEAx143bjGHa9s0l7ESwwgpwC0ydPcSfk",
+    authDomain: "carbonzero-sl.firebaseapp.com",
+    projectId: "carbonzero-sl"
 };
 
 initializeApp(config);
