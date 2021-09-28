ea84c3c7f71befd984a158aebc826fb27f50b352
Bryan Soong <whiterevolver35@gmail.com>
2021-09-28 21:09:06 +0800

    :sparkles: updated meta

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,19 @@
 import Head from 'next/head';
-import Image from'next/image';
+import { NextSeo } from 'next-seo';
 import Footer from './Footer';
 import NavigationBar from './NavBar';
-import OGImage from '../public/ogimage.png';
 
 export default function Layout({ children }) {
   return (
     <>
         <Head>
+          <NextSeo 
+            title="Carbon Zero - MCKL Service Learning"
+          />
             <title>Carbon Zero - MCKL Service Learning</title>
             <meta property="og:title" content="Carbon Zero - MCKL Service Learning" />
             <meta property="og:description" content="Carbon Zero is a service learning project carried out by students of MCKL to raise awareness on carbon footprint." />
-            <meta property="og:image" content={<Image src={OGImage} alt="og"/>} />
+            <meta property="og:image" content="https://i.ibb.co/ZxgwW14/ogimage.png" />
             <meta property="description" content="Carbon Zero is a service learning project carried out by students of MCKL to raise awareness on carbon footprint." />
             <meta name="keywords" content="environment, carbon footprint, save the planet" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
